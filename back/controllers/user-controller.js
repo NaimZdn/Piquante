9d4d161e7a1738c6f917d9377b7e0d4abe656649
Naïm Zidouni <naim.zidouni@outloook.fr>
2022-06-09 20:02:16 +0200

    add multer

--- back/controllers/user-controller.js
+++ back/controllers/user-controller.js
@@ -31,7 +31,9 @@ exports.login = (req, res, next) => {
                     res.status(200).json({ 
                         userId : user._id, 
                         token: webToken.sign( 
-                            {userId: user._id }
+                            {userId: user._id }, 
+                            'RANDOM_TOKEN_SECRET', 
+                            { expiresIn: '24h' }
                         )
                     });
                 })
