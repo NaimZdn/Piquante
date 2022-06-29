d7b96e2f0db0f430f1229b72362de6e37e6ad7fb
Naïm Zidouni <naim.zidouni@outloook.fr>
2022-06-29 11:36:54 +0200

    project final

--- back/middlewares/multer-config.js
+++ back/middlewares/multer-config.js
@@ -1,15 +1,19 @@
+// Importation de multer qui nous permet d'enregistrer les images que les utilisateurs vont upload dans sur notre application
 const multer = require('multer');
 
+// Création des extensions des fichiers.
 const MIME_TYPES = {
   'image/jpg': 'jpg',
-  'image/jpeg': 'jpg',
+  'image/jpeg': 'jpeg',
   'image/png': 'png'
 };
 
+// On dit un multer que l'on va enregister les images sur notre disque, ici ce sera le dossier images.
 const storage = multer.diskStorage({
   destination: (req, file, callback) => {
     callback(null, 'images');
   },
+  // On dit a multer quel nom de fichier utiliser. On enlever les espaces en les remplaçant par des underscore.
   filename: (req, file, callback) => {
     const extension = MIME_TYPES[file.mimetype];
     const name = file.originalname.split('.' + extension).join('_');
@@ -17,4 +21,5 @@ const storage = multer.diskStorage({
   }
 });
 
+// On exporte l'objet storage.
 module.exports = multer({storage: storage}).single('image');
